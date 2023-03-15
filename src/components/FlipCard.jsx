9565790013dcf49e0438719c0e4ffffc92a12919
Trek182021 <82979983+Trek182021@users.noreply.github.com>
2023-03-15 12:12:18 -0700

    Update Part 3

--- src/components/FlipCard.jsx
+++ src/components/FlipCard.jsx
@@ -1,22 +1,51 @@
-
+import {useState} from 'react';
 
 export default function FlipCard({data}) {
+
     
-    let choices = [...data.incorrectAnswers, data.correctAnswer]
+    let choices = [...data.incorrectAnswers, data.correctAnswer];
+    const correctAnswer = data.correctAnswer.toLowerCase();
+    const [guess, setGuess] = useState("");
+    const [submit, setSubmit] = useState(false);
+    const [answer, setAnswer] = useState(false);
+    const [isCorrect, setIsCorrect] = useState(false)
 
     // Shuffle array to avoid having correct answer in the same spot
-    choices.sort(() => Math.random() - 0.5)
+    // choices.sort(() => Math.random() - 0.5)
+
+
+    const handleSubmit = () => {
+      if (guess != "") { 
+        setSubmit(true);
+        setAnswer(true);
+        // console.log(answer)
+        
+        // console.log("Guess: " + typeof(guess));
+        // console.log("Answer: " + typeof(correctAnswer));
+        // console.log(data.correctAnswer.toLowerCase())
+        var run = true;
+        for(var i = 0; i < correctAnswer.length -1;i++) {
+          if (correctAnswer[i].toLowerCase != guess[i].toLowerCase) {
+            run = false;
+          }
+        }
+        setIsCorrect(run);
+      
+        
+      }
+      
+    }
 
     return (
       <>
-        <div className="flip-card">
+        <div className={"flip-card " + (submit ? "" : "disabled")}>
 
-          <div className="flip-card-inner">
+          <div className={`flip-card-inner ${(answer) ? (isCorrect ? "correct" : "wrong"): ""}`}>
             <div className="flip-card-front">
                 <h2>{data.question}</h2>
 
                 <div className="incorrectAnswers">
-                    {choices.map( answer => <p>{answer}</p>)}
+                    {choices.map( (answer, index) => <p key={index}>{answer}</p>)}
                 </div>
             </div>
 
@@ -26,6 +55,11 @@ export default function FlipCard({data}) {
 
           </div>
 
+        </div>
+        <div className="guess-input">
+            <input type="text" id="guess" value={guess} onChange={e => setGuess(e.target.value)} placeholder="Enter Guess here..."required></input>
+            <button type="submit" onClick={handleSubmit}>Submit Guess</button>
+         
         </div>
       </>
     )
